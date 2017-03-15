2b62d3a89b27c5fdda412f86fe3ce82118e83316
lhmelo <laurahmelo@gmail.com>
2017-03-15 15:56:15 -0700

    updated to log2ratio consistent with rest of workflow; includes "Raw Data" in saved output since logplot function is typically used to plot rawdata

--- logplot.function.R
+++ logplot.function.R
@@ -1,3 +1,13 @@
+#logplot()  
+#Input:
+#1. xset.filtered, which has already been filtered against the blanks.
+#
+#Output:
+#1. logplotdata.csv: data derived from xset.filtered which has been filtered to remove rows where p > 0.05.
+#
+#logplot() performs a t test for each mass feature, retains only mass features which display signficant distributions between samples (p < 0.05) and calculates a new "logratio".  
+
+
 logplot <- function (sigcutoff = 0.05){
   
   for (j in 1:length(FractionList)) {
@@ -37,13 +47,13 @@ logplot <- function (sigcutoff = 0.05){
     data$AveTreat2 <- rowMeans(data[, Treat2])
     #test <- data[with(data, order(-data$Ave9313)),]
     
-    topTreat1 <- data %>% arrange(desc(data$AveTreat1)) %>% head(50)
+    topTreat1 <- data %>% arrange(desc(AveTreat1)) %>% head(50)
     #top9313 <- top_n(data, 50, data$Ave9313)
     write.csv(topTreat1,
               file = paste(Fraction, "top", Treat1ID, "csv", sep = "."),
               row.names = FALSE)
     #          
-    topTreat2 <- data %>% arrange(desc(data$AveTreat2)) %>% head(50)         
+    topTreat2 <- data %>% arrange(desc(AveTreat2)) %>% head(50)         
     #top9312 <- top_n(data, 50, data$Ave9312)
     write.csv(topTreat2,
               file = paste(Fraction, "top", Treat2ID, "csv", sep = "."),
@@ -53,39 +63,46 @@ logplot <- function (sigcutoff = 0.05){
     #top9313_9312 <- intersect(top9312$groupname, top9313$groupname)
     #
     
-    sig.groupnames <- subset(data, data$pvalue < sigcutoff)
-    sig.groupnames$AveTreat1 <- rowMeans(sig.groupnames[, Treat1])
-    sig.groupnames$AveTreat2 <- rowMeans(sig.groupnames[, Treat2])
+   
     
     #building "logratio" which will represent enrichment of 9313 mass features as positive values and encrichment of 9312 mass features as negative values
     #
     #sig.groupnames$logratio <- NA
     
-    sig.groupnames$logratio <-
+    data$log2ratio <-
       ifelse(
-        sig.groupnames$AveTreat1 > sig.groupnames$AveTreat2,
-        log10(sig.groupnames$AveTreat1 / sig.groupnames$AveTreat2),
+       data$AveTreat1 > data$AveTreat2,
+        log2(data$AveTreat1 / data$AveTreat2),
         ifelse(
-          sig.groupnames$AveTreat1 < sig.groupnames$AveTreat2,
-          -(log10(
-            sig.groupnames$AveTreat2 / sig.groupnames$AveTreat1
+          data$AveTreat1 < data$AveTreat2,
+          -(log2(
+            data$AveTreat2 / data$AveTreat1
           )),
           "other"
         )
       )
     
-    #replacing Inf with "4" in order to treat the column as numeric
-    sig.groupnames$logratio <- gsub('Inf', '5', sig.groupnames$logratio)
-    sig.groupnames$logratio <- as.numeric(sig.groupnames$logratio)
+   data$log2ratio <- gsub('Inf', '25', data$log2ratio)
+    data$log2ratio <- as.numeric(data$log2ratio)
     
-    write.csv(sig.groupnames, paste(Fraction, "logplotdata", "csv", sep="."))
+    sig.groupnames <- subset(data, data$pvalue < sigcutoff)
+    # sig.groupnames$AveTreat1 <- rowMeans(sig.groupnames[, Treat1])
+    # sig.groupnames$AveTreat2 <- rowMeans(sig.groupnames[, Treat2])
     
-    p <- ggplot(sig.groupnames, aes(x = mz, y = logratio, color = pvalue)) +
+    #replacing Inf with "25" in order to treat the column as numeric
+    sig.groupnames$log2ratio <- gsub('Inf', '25', sig.groupnames$log2ratio)
+    sig.groupnames$log2ratio <- as.numeric(sig.groupnames$log2ratio)
+    
+    write.csv(data, paste(Fraction, "logplotdata", "csv", sep="."))
+    
+    p <- ggplot(sig.groupnames, aes(x = mz, y = log2ratio, color = pvalue)) +
       geom_point() +
       scale_color_continuous(low = "red", high = "blue") +
-      labs(title = (paste(Fraction, Experiment, sep = " "))) +
+      labs(title = (paste(Fraction, Experiment, "Raw Data", sep = " "))) +
       theme_classic()
     
     print(p)
+    
+    save_plot(paste(Fraction, Experiment, "RawDataLogPlot.pdf", sep="."), p,  base_aspect_ratio = 2)
 }
 }
